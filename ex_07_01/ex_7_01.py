7eef93902b5368c7c23b67ad5887dd0702c27b21
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-06-23 15:36:50 -0500

    comments

--- ex_07_01/ex_7_01.py
+++ ex_07_01/ex_7_01.py
@@ -2,5 +2,8 @@ fh = open('mbox-short.txt')
 # print(fh)
 
 for lx in fh:
+    # rstrip() method removes the line ending characters to make the doc more readable
     ly = lx.rstrip()
+    # .upp() method changes all text to uppercase, .lower() does opposite
     print(ly.upper())
+
